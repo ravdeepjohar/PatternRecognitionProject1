e934fde8716af3b788cd94f3f7286fd121e37380
Ravdeep Johar <ravdeepsinghjohar@live.com>
2013-10-22 01:52:55 -0400

    add parser.py

---
+++ parser.py
@@ -0,0 +1,34 @@
+import xml.etree.ElementTree as ET
+
+
+#f = open('AllEM_part4_TRAIN_all.txt')
+
+#for line in f:
+    
+#tree = ET.parse(line)
+tree = ET.parse('../expressmatch/65_alfonso.inkml')
+root = tree.getroot()
+    
+
+xcor = []
+ycor = []     
+for neighbor in root.iter('{http://www.w3.org/2003/InkML}trace'):
+    t = neighbor.text.split(",")
+    x = []
+    y = []
+    for cor in t:
+        if cor.startswith(" "):
+            cor = cor.strip()
+        x.append(cor.split(" ")[0]) 
+        y.append(cor.split(" ")[1])
+    xcor.append(x)
+    ycor.append(y)
+   
+    
+for neighbor in root.iter('{http://www.w3.org/2003/InkML}traceGroup'):
+    for n in neighbor.iter('{http://www.w3.org/2003/InkML}traceView'):
+        print n.attrib["traceDataRef"]
+        
+    print " "
+         
+#f.close()
