9d3d2a4150cccf794e8a70b7dc6a151f9747276f
Ravdeep Johar <ravdeepsinghjohar@live.com>
2013-10-22 20:00:25 -0400

    add stuff

--- parser.py
+++ parser.py
@@ -9,6 +9,12 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('../expressmatch/65_alfonso.inkml')
 root = tree.getroot()
     
+def bernstein_poly(i, n, t):
+    """
+     The Bernstein polynomial of n, i as a function of t
+    """
+    return comb(n, i) * ( t**(n-i) ) * (1 - t)**i
+    
 
 xcor = []
 ycor = []     
@@ -32,6 +38,47 @@ for neighbor in root.findall('{http://www.w3.org/2003/InkML}traceGroup'):
               indices = []
               for n in n.iter('{http://www.w3.org/2003/InkML}traceView'):
                   indices.append(int(n.attrib["traceDataRef"]))
+
+# Remove Duplicates 
+newxcor = []
+newycor = []                
+for xc,yc in zip(xcor,ycor): 
+    newx = []
+    newy = []              
+    for x, y in zip(xc, yc):
+        t = True
+        for x1c,y1c in zip(newx,newx):
+            if x==x1c and y==y1c:
+                t = False
+        if (t):
+            newx.append(x)
+            newy.append(y)
+    newxcor.append(newx)
+    newycor.append(newy)
+    
+xcor = newxcor
+ycor = newycor
+
+# Benzier Curve
+
+cors = []
+for xc,yc in zip(xcor,ycor):
+    cor = []
+    for x,y in zip(xc,yc):           
+        cor.append([x,y])
+    cors.append(cor)
+    
+
+
+
+
+
+            
+            
+       
+    
+    
+    
                   
              
                   
