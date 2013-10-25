99f70dd33516aec5b206517bd2134c76ac94b56a
Piyush Verma <pxv8270@rit.edu>
2013-10-24 21:06:42 -0400

    Feature Vector Calculation
    Calculated feature vectors including aspect ratio, normalized y
    coordinates, vicinity slope, curvature and number of strokes

--- parser.py
+++ parser.py
@@ -3,6 +3,7 @@ import xml.etree.ElementTree as ET
 import numpy as np
 from scipy.misc import comb
 import matplotlib.pyplot as plt
+import math
 
 
 #f = open('AllEM_part4_TRAIN_all.txt')
@@ -10,7 +11,7 @@ import matplotlib.pyplot as plt
 #for line in f:
     
 #tree = ET.parse(line)
-tree = ET.parse('../expressmatch/98_alfonso.inkml')
+tree = ET.parse('../expressmatch/79_edwin.inkml')
 root = tree.getroot()
     
 def bernstein_poly(i, n, t):
@@ -87,36 +88,129 @@ for sym in symbol_dict:
         
         temp.append(cor)
        
-        plt.plot(xvals, yvals)
+        #plt.plot(xvals, yvals)
         itr = itr + 1
         sums = sums + nTimes
     
   
     points_dict[sym]=temp
     
+#calculate aspect ratio for each symbol
+
+aspect_ratio = []
+    
+for i in points_dict:
+    max_width = 0.0
+    max_height = 0.0
+    min_width = 0.0
+    min_height = 0.0
+    #print i
+    for j in range(len(points_dict[i])):
+        for coord in points_dict[i][j]:
+            if(max_width == 0.0):
+                max_width = coord[0]
+            elif(max_width < coord[0]):
+                max_width = coord[0]
+            
+            if(max_height == 0.0):
+                max_height = coord[1]
+            elif(max_height < coord[1]):
+                max_height = coord[1]
+            
+            if(min_width == 0.0):
+                min_width = coord[0]
+            elif(min_width > coord[0]):
+                min_width = coord[0]
+            
+            if(min_height == 0.0):
+                min_height = coord[1]
+            elif(min_height > coord[1]):
+                min_height = coord[1]
     
     
+    '''print max_width
+    print min_width
+    print max_height
+    print min_height
+    print '\n'''
+
     
+    aspect = (max_width - min_width)/(max_height - min_height)
+    aspect_ratio.append(aspect)
     
 
+#calculate normalized y coordinates
 
+normalized_Y = []
 
-
-
+for i in points_dict:
+    tempYList = []
+    for j in range(len(points_dict[i])):
+        for coord in points_dict[i][j]:
+            mag = np.sqrt(coord[0] * coord[0] + coord[1] * coord[1])
+            temp = coord[1] / mag
+            tempYList.append(temp)
+    
+    normalized_Y.append(tempYList)
     
 
+#calculate vicinity slope pairs
 
+vicinity_slope = []
 
+for i in points_dict:
+    tempSlope = []
+    tempList = []
+    for j in range(len(points_dict[i])):
+        for coord in points_dict[i][j]:
+            tempList.append(coord)
+    
+    for k in range(2, len(tempList)-2):
+        horizontal = [1.0, 0.0]
+        vec = [tempList[k+2][0] - tempList[k-2][0], tempList[k+2][1] - tempList[k-2][1]]
+        magHorizonal = np.sqrt(horizontal[0] * horizontal[0] + horizontal[1] * horizontal[1])
+        magVec = np.sqrt(vec[0] * vec[0] + vec[1] * vec[1])
+        cosTheta = (horizontal[0] * vec[0] + horizontal[1] * vec[1])/(magVec * magHorizonal)
+        sinTheta = math.sin(math.acos(cosTheta))
+        tempSlope.append([sinTheta, cosTheta])
+    
+    vicinity_slope.append(tempSlope)
+    
+#calculate curvature
+    
+curvature = []
+    
+for i in points_dict:
+    tempList = []
+    tempCurvature = []
+    for j in range(len(points_dict[i])):
+        for coord in points_dict[i][j]:
+            tempList.append(coord)
+    
+    for k in range(2, len(tempList) - 2):
+        vec1 = [tempList[k-2][0] - tempList[k][0], tempList[k-2][1] - tempList[k][1]]
+        mag1 = np.sqrt(vec1[0] * vec1[0] + vec1[1] * vec1[1])
+        vec2 = [tempList[k][0] - tempList[k+2][0], tempList[k][1] - tempList[k+2][1]]
+        mag2 = np.sqrt(vec2[0] * vec2[0] + vec2[1] * vec2[1])
+        
+        cosTheta = (vec1[0] * vec2[0] + vec1[1] * vec2[1])/(mag1 * mag2)
+        sinTheta = math.sin(math.acos(cosTheta))
+        
+        tempCurvature.append([sinTheta, cosTheta])
+    
+    curvature.append(tempCurvature)
+    
 
+#calculate number of strokes
+
+numStrokes = []
+for i in points_dict:
+    num = 0
+    for j in range(len(points_dict[i])):
+        num += 1
+    numStrokes.append(num)
 
             
-            
-       
-    
-    
-    
-                  
-             
                   
               
 
